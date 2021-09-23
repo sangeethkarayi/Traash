3d0cb9012a8bff548559bc4704f161250a6b3acc
Sangeeth Karayi <72119067+sangeethkarayi@users.noreply.github.com>
2021-09-23 13:15:17 +0530

    Nucked PIxel SetupWizard

--- config.mk
+++ config.mk
@@ -130,7 +130,6 @@ PRODUCT_PACKAGES += \
     GoogleOneTimeInitializer \
     GoogleServicesFramework \
     grilservice \
-    PixelSetupWizard \
     RilConfigService \
     StorageManagerGoogle
 
