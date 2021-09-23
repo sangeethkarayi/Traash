8be7d38558886df4a5dc37b08acc991c500f64fa
Sangeeth Karayi <72119067+sangeethkarayi@users.noreply.github.com>
2021-09-23 13:13:22 +0530

    X00TD:Nucked SetupWizard

--- config.mk
+++ config.mk
@@ -102,7 +102,6 @@ PRODUCT_PACKAGES += \
     Phonesky \
     SafetyHubPrebuilt \
     ScribePrebuilt \
-    SetupWizardPrebuilt \
     Showcase \
     TetheringEntitlement \
     USCCDM \
@@ -151,15 +150,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librsjni
-
-# SetupWizard
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_v3_light \
-    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
-    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
-    setupwizard.feature.show_pixel_tos=true \
-    setupwizard.feature.baseline_setupwizard_enabled=true \
-    ro.setupwizard.esim_cid_ignore=00000001
 
 # System whitelist
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
